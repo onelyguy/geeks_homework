a4c077669e6ea7607b02f2530a2692b457040dfa
onelyguy <147728048+onelyguy@users.noreply.github.com>
2023-11-14 22:02:19 +0600

    Homework #2

--- project_program_for_students/js/home_works.js
+++ project_program_for_students/js/home_works.js
@@ -5,7 +5,7 @@ const gmailInput = document.querySelector('#gmail_input');
 const gmailButton = document.querySelector('#gmail_button');
 const gmailSpan = document.querySelector('#gmail_result');
 
-const regExp = /@gmail.com$/
+const regExp = /^[a-zA-Z0-9._%+-]+@gmail\.com$/
 
 gmailButton.addEventListener('click', () => {
     if (regExp.test(gmailInput.value)) {
@@ -20,14 +20,61 @@ gmailButton.addEventListener('click', () => {
 // Task 2
 
 const square = document.querySelector('.child_block');
-let movement = 0;
+let positionX = 0;
+let positionY = 0;
 
 const moveSquare = () => {
-    if (movement < 448) {
-        movement++
-        square.style.left = `${movement}px`
-        setTimeout(moveSquare, 10)
+    if (positionX < 448 && positionY === 0) {
+        positionX++;
+        square.style.left = `${positionX}px`;
+        setTimeout(moveSquare, 10);
+    } else if (positionX >= 448 && positionY < 448) {
+        positionY++;
+        square.style.top = `${positionY}px`;
+        setTimeout(moveSquare, 10);
+    } else if (positionX > 0 && positionY >= 448) {
+        positionX--;
+        square.style.left = `${positionX}px`;
+        setTimeout(moveSquare, 10);
+    } else if (positionX === 0 && positionY > 0) {
+        positionY--;
+        square.style.top = `${positionY}px`;
+        setTimeout(moveSquare, 10);
     }
 }
 
 moveSquare();
+
+
+// Homework 2
+
+
+const start = document.querySelector('#start');
+const stop = document.querySelector('#stop');
+const reset = document.querySelector('#reset');
+const seconds = document.querySelector('#secondsS');
+let num = 0;
+let interval;
+let isRunning = false;
+
+start.addEventListener('click', () => {
+    if (!isRunning) {
+        isRunning = true;
+        interval = setInterval(() => {
+            num++;
+            seconds.innerHTML = num;
+        }, 1000);
+    }
+});
+
+stop.addEventListener('click', () => {
+    clearInterval(interval);
+    isRunning = false;
+});
+
+reset.addEventListener('click', () => {
+    clearInterval(interval);
+    isRunning = false;
+    num = 0;
+    seconds.innerHTML = num;
+});
